e6eab711a599f296285dd09a198b7942aca787dd
Collin Wheat <collinwheat@gmail.com>
2025-02-27 15:44:45 -0500

    fixing app.module.ts from header and footer creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/home/header/header.component';
-import { FooterComponent } from './components/home/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
