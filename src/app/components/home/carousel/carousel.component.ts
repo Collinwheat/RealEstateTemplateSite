a4658b239011587175b0b6e0a027faa91a7b0c3c
Collin Wheat <collinwheat@gmail.com>
2025-03-04 10:40:04 -0500

    updating angular wip

--- src/app/components/home/carousel/carousel.component.ts
+++ src/app/components/home/carousel/carousel.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
-@Component({
+ ({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css'],
