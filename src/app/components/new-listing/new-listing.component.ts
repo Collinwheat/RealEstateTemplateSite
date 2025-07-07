5f106a4fc3cb4e1e473afebdd31018a949a55034
Collin Wheat <collinwheat@gmail.com>
2025-07-07 12:05:51 -0400

    updating new-listing to work better for adding to current listings

--- src/app/components/new-listing/new-listing.component.ts
+++ src/app/components/new-listing/new-listing.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio'
+import { Listing } from 'src/app/models/listing/listing';
 
 @Component({
   selector: 'app-new-listing',
@@ -8,12 +10,30 @@ import { MatRadioModule } from '@angular/material/radio'
   templateUrl: './new-listing.component.html',
   styleUrl: './new-listing.component.css'
 })
-export class NewListingComponent {
+export class NewListingComponent implements OnInit{
 
   @Output() close = new EventEmitter<void>();
+  listing: Listing = new Listing();
+  newListingForm: FormGroup | undefined;
+
+  ngOnInit(): void {
+      this.newListingForm = new FormGroup({
+        address: new FormControl('', Validators.required),
+        price: new FormControl('', Validators.required),
+        bedrooms: new FormControl('', Validators.required),
+        bathrooms: new FormControl('', Validators.required),
+        linkToListing: new FormControl('', Validators.required),
+        status: new FormControl('', Validators.required),
+        listingImage: new FormControl(''),
+      })
+  }
 
   closeModal(): void {
 	this.close.emit();
+  }
+
+  submitNewListing() {
+
   }
 
 }
